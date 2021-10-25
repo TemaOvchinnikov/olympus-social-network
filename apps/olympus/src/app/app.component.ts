f5a5b2f34afa0a9ca2daa52e1b5332e24e136e6e
Tema Ovchinnikov <flatsplit18@gmail.com>
2021-10-25 21:17:52 +0300

    initial commit

---
+++ apps/olympus/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'olympus-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  title = 'olympus';
+}
