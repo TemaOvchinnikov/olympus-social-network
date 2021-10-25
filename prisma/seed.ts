8e58d83535a3718234f5f79e7e70aa69c91fa86f
Tema Ovchinnikov <flatsplit18@gmail.com>
2021-10-25 22:45:33 +0300

    prisma seed user

---
+++ prisma/seed.ts
@@ -0,0 +1,28 @@
+﻿import { PrismaClient } from '@prisma/client';
+import { getMaxListeners } from 'process';
+
+const prisma = new PrismaClient();
+
+const items = [
+  {
+    email: 'user1@gmail.com',
+    name: 'user1',
+  },
+  {
+    email: 'user2@gmail.com',
+    name: 'user2',
+  },
+];
+
+async function main() {
+  for (let item of items) {
+    await prisma.user.create({ data: item });
+  }
+}
+
+main()
+  .catch((e) => {
+    console.log(e);
+    process.exit(1);
+  })
+  .finally(() => prisma.$disconnect());
