11d4fe965fb796441b7d6757620e4ff2ac010c69
Tema Ovchinnikov <flatsplit18@gmail.com>
2021-10-26 00:58:37 +0300

    add prisma migrate posts and posts service

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PostsController, PostsService } from '@olympus/posts';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, PostsController],
+  providers: [AppService, PostsService],
 })
 export class AppModule {}
--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,22 +1,87 @@
 ﻿import { PrismaClient } from '@prisma/client';
-import { getMaxListeners } from 'process';
 
 const prisma = new PrismaClient();
 
-const items = [
+const users = [
   {
     email: 'user1@gmail.com',
-    name: 'user1',
+    name: 'user1'
   },
   {
     email: 'user2@gmail.com',
-    name: 'user2',
+    name: 'user2'
+  },
+];
+
+const posts = [
+  {
+    title: 'post 1',
+    authorId: 1,
+  },
+  {
+    title: 'post 2',
+    authorId: 1,
+  },
+  {
+    title: 'post 3',
+    authorId: 2,
   },
 ];
 
 async function main() {
-  for (let item of items) {
-    await prisma.user.create({ data: item });
+  for (let _user of users) {
+    await prisma.user.create({ data: _user });
+  }
+
+  for (let _post of posts) {
+    await prisma.post.create({ data: _post });
+  }
+}
+
+main().catch((e) => console.error(e));
+
+/*
+const users = [
+  {
+    email: 'user1@gmail.com',
+    name: 'user1',
+    posts: 
+  },
+  {
+    email: 'user2@gmail.com',
+    name: 'user2',
+    posts: 
+  },
+];
+
+const posts = [
+  {
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    title: 'post2',
+    author: 'user1',
+    authorId: 1
+  },
+  {
+    createdAt: new Date(),
+    updatedAt: new Date(),
+    title: 'post2',
+    author: 'user1',
+    authorId: 1,
+  },
+];
+
+async function main() {
+  for (let _user of users) {
+    await prisma.user.create({ data: _user });
+  }
+
+  for (let _post of posts) {
+    await prisma.user.create({ data: _post });
+  }
+
+  for (let _post of posts) {
+    await prisma.post.create({ data: _post });
   }
 }
 
@@ -26,3 +91,4 @@ main()
     process.exit(1);
   })
   .finally(() => prisma.$disconnect());
+  */
